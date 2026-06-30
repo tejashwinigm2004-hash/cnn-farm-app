d4b37edb7d36bbc3bd0cb0e00eccbd48423afcd1
Narasimha Murthy C N <nnarasimhamurthycn@gmail.com>
2026-06-30 16:20:56 +0530

    Convert app.json to app.config.js for env variable support

---
+++ app.config.js
@@ -0,0 +1,45 @@
+export default {
+  expo: {
+    name: "CNN Milk",
+    slug: "cnn-farm-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/logo.png",
+    scheme: "cnnfarmapp",
+    userInterfaceStyle: "automatic",
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#143219",
+        foregroundImage: "./assets/logo.png"
+      },
+      predictiveBackGestureEnabled: false,
+      package: "com.cnnmilk.app",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json"
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/logo.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          backgroundColor: "#143219",
+          image: "./assets/logo.png",
+          imageWidth: 200
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "8cfb4954-5b58-4ba5-aff8-de3ca41761fd"
+      }
+    }
+  }
+};
